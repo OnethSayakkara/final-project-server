c48a876708e10164b78f166e2da9f80602be6671
oneth sayakkara <onethsayakkara@gmail.com>
2025-08-04 22:13:54 +0530

    long time

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ import express from 'express';
 import { config } from 'dotenv';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 config(); // Initialize dotenv to load environment variables
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors({
+  origin: 'http://localhost:5173', 
+  credentials: true, 
+}));
 
 app.use(express.json());
 app.use(cookieParser());
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,7 +5,7 @@ import { authMiddleware } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.post('/registeruser', register);
-router.post('/getallusers', authMiddleware, getAllUsers);
+router.post('/getallusers', getAllUsers);
 router.post('/getuserbyid/:id', authMiddleware, getUserById);
 router.post('/updateuser/:id', authMiddleware, updateUser);
 router.post('/deleteUser/:id', authMiddleware, deleteUser);
